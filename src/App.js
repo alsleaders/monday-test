cac55a63e327333a38ef614ddf69c640af09528f
Amanda Leaders <alsleaders@gmail.com>
2019-07-01 19:47:46 -0400

    front page styled

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react'
 import axios from 'axios'
 import ReactMapGL, { Marker, Popup, NavigationControl } from 'react-map-gl'
 // import 'mapbox-gl/dist/mapbox-gl.css'
+import Header from './components/Header'
 
 const TOKEN =
   'pk.eyJ1IjoiYWxzbGVhZGVycyIsImEiOiJjang1aXNrcGkwMmR5M3lsZzg4OXFyNWRqIn0.qQib-cz84tOegHyTyc0U9g'
@@ -9,13 +10,17 @@ const TOKEN =
 export default function HelloWorld() {
   const [view, setView] = useState({
     latitude: 27.9506,
-    // this is 90
+    // this is up to 90
     longitude: -82.4572,
-    // this is 360
+    // this is up to 180
     zoom: 4
   })
   const [selectedInfo, setSelectedInfo] = useState(null)
   const [mapData, setMapData] = useState([])
+  const [currentLocation, setCurrentLocation] = useState('')
+  const [plannedDestination, setPlannedDestination] = useState('')
+
+  // get current location and planned destination, geocode into long & lat, drop pins on the map, and post the location.
 
   useEffect(() => {
     axios.get('https://localhost:5001/api/location').then(resp => {
@@ -26,10 +31,11 @@ export default function HelloWorld() {
 
   return (
     <>
+      <Header />
       <ReactMapGL
         {...view}
-        width="100vw"
-        height="100vh"
+        width="85vw"
+        height="60vh"
         mapStyle="mapbox://styles/mapbox/satellite-streets-v11"
         mapboxApiAccessToken={TOKEN}
         onViewportChange={view => {
@@ -66,6 +72,26 @@ export default function HelloWorld() {
           </Popup>
         ) : null}
       </ReactMapGL>
+      <hr />
+      <section className="input-fields">
+        <h3>Input your location and destination</h3>
+        <div className="input-column">
+          <input
+            type="text"
+            placeholder="Current location"
+            value={currentLocation}
+            onChange={e => setCurrentLocation(e.target.value)}
+          />
+          {/* </div>
+        <div className="input-column"> */}
+          <input
+            type="text"
+            placeholder="Planned destination"
+            value={plannedDestination}
+            onChange={e => setPlannedDestination(e.target.value)}
+          />
+        </div>
+      </section>
     </>
   )
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,14 @@
 import React, { Component } from 'react'
 
-class HelloWorld extends Component {
+class Header extends Component {
   render() {
     return (
-      <>
+      <main className="header">
         <h1>Monday Map Test</h1>
         <p> because alliteration is alarmingly addictive</p>
-      </>
+      </main>
     )
   }
 }
 
-export default HelloWorld
+export default Header
